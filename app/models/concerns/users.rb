4bd2fb045a07cb99bcdc61c17b2f07f998106d27
yoshihiro asada <yoshihiro0921@gmail.com>
2015-11-29 17:33:34 +0900

    Implement module to connect intarnal api

---
+++ app/models/concerns/users.rb
@@ -0,0 +1,22 @@
+module Users
+  class Me
+    attr_reader :id, :last_name, :first_name
+
+    def initialize(params)
+      @id = params[:id]
+      @last_name = params[:last_name]
+      @first_name = params[:first_name]
+    end
+  end
+
+  class User_info
+    attr_reader :id, :last_name, :first_name, :image
+
+    def initialize(params)
+      @id = params[:id]
+      @last_name = params[:last_name]
+      @first_name = params[:first_name]
+      @image = params[:image]
+    end
+  end
+end
