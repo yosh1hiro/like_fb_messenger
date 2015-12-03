65c379160f7466527a79063bafbf15adf239bc2f
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-03 18:34:27 +0900

    Fix code

--- app/models/concerns/users.rb
+++ app/models/concerns/users.rb
@@ -1,43 +1,22 @@
 module Users
-  class Me
-    attr_reader :id, :last_name, :first_name
 
-    def initialize(params = {})
-      @id = params["id"]
-      @last_name = params["last_name"]
-      @first_name = params["first_name"]
-    end
+  def me(access_token, version="v1")
 
-    def to_hash
-      { id: @id,
-        last_name: @last_name,
-        first_name: @first_name
-      }
-    end
   end
 
-  class Other
-    attr_reader :id, :last_name, :first_name, :image
-
-    def initialize(params = {})
-      @id = params["id"]
-      @last_name = params["last_name"]
-      @first_name = params["first_name"]
-      @image = params["image"]
-    end
-
-    def name
-      @last_name + @first_name
-    end
-
-    def to_hash
-      {
-        id: @id,
-        name: name,
-        image: @image
-      }
-    end
+  def user_info(access_token, user_id, version="v1")
+    Users::Other.new()
   end
 
+  def candidates(access_token, page="1", version="v1")
+    candidates = []
+    users = @requl_mobile_api.candidates(access_token, page, version)
+    users["users"].each { |user| candidates << Users::Other.new(user) }
+    candidates
+  end
 
+  def cendidates_end_flag(access_token, page="1", version="v1")
+    candidate = @requl_mobile_api.candidates(access_token, page, version)
+    candidate["end_flag"]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,62 @@
-class User
-  def initialize
-    @requl_mobile_api = RequlMobileUsersApi.new
+module User
+  class Me
+    include ChatModule
+
+    attr_reader :id, :last_name, :first_name
+
+    def initialize
+      @requl_mobile_api = RequlMobileUsersApi.new
+    end
+
+    def to_hash
+      {
+        id: @id,
+        last_name: @last_name,
+        first_name: @first_name
+      }
+    end
+
+    def me(access_token, version="v1")
+      res = @requl_mobile_api.me(access_token, version)
+      @id = res["id"]
+      @last_name = res["last_name"]
+      @first_name = res["first_name"]
+    end
+
+    def candidates(access_token, page="1", version="v1")
+      candidates = []
+      users = @requl_mobile_api.candidates(access_token, page, version)
+      users["users"].each { |user| candidates << Other.new(user) }
+      candidates
+    end
   end
 
-  def me(access_token, version="v1")
-    Users::Me.new(@requl_mobile_api.me(access_token, version))
-  end
+  class Other
+    attr_reader :id, :last_name, :first_name, :image
 
-  def user_info(access_token, user_id, version="v1")
-    Users::Other.new(@requl_mobile_api.user_info(access_token, user_id, version))
-  end
+    def initialize(params = {})
+      @requl_mobile_api = RequlMobileUsersApi.new
+      @id = params["id"]
+      @last_name = params["last_name"]
+      @first_name = params["first_name"]
+      @image = params["image"]
+    end
 
-  def candidates(access_token, page="1", version="v1")
-    candidates = []
-    users = @requl_mobile_api.candidates(access_token, page, version)
-    users["users"].each { |user| candidates << Users::Other.new(user) }
-    candidates
-  end
+    def to_hash
+      {
+        id: @id,
+        last_name: @last_name,
+        first_name: @first_name,
+        image: @image
+      }
+    end
 
-  def cendidates_end_flag
-    candidate = @requl_mobile_api.candidates(access_token, page, version)
-    candidate["end_flag"]
+    def othere
+      res = @requl_mobile_api.user_info(access_token, user_id, version)
+      @id = res["id"]
+      @last_name = res["last_name"]
+      @first_name = res["first_name"]
+      @image = res["image"]
+    end
   end
 end
