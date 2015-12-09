70142073c80244b7590c16b85742ce213bb94e36
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-09 20:31:10 +0900

    Implement ChatDirectWithAdminRoom.find_or_create_by

--- app/models/chat_direct_with_admin_room.rb
+++ app/models/chat_direct_with_admin_room.rb
@@ -20,5 +20,19 @@ class ChatDirectWithAdminRoom < ActiveRecord::Base
   has_many :chat_direct_with_admin_from_admin_images, dependent: :destroy
   has_many :chat_direct_with_admin_stamps, dependent: :destroy
   has_many :chat_direct_with_admin_from_admin_stamps, dependent: :destroy
-  has_one :chat_room_index_caches, as: :chat_room, dependent: :destroy
+  has_one :chat_room_index_cache, as: :chat_room, dependent: :destroy
+
+  def target_type
+    'Admin'
+  end
+
+  class << self
+    def find_or_create_by(user, admin)
+      chat_room = ChatDirectWithAdminRoom.find_or_initialize_by(user_id: user.id, admin_id: admin.id)
+      return chat_room if chat_room.persisted?
+
+      chat_room.save!
+      chat_room
+    end
+  end
 end
