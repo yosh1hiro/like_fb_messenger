e35b102fb55f33ec1f998e496e81f1edac278361
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-09 20:30:13 +0900

    Write specs for ChatDirectWithAdminRoom.find_of_create_by

--- spec/models/chat_direct_with_admin_room_spec.rb
+++ spec/models/chat_direct_with_admin_room_spec.rb
@@ -16,5 +16,38 @@
 require 'rails_helper'
 
 RSpec.describe ChatDirectWithAdminRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.find_or_create_by' do
+    let(:user_params1) do
+      {
+        'id' => 1,
+        'last_name' => 'Ritenour',
+        'first_name' => 'Lee',
+        'access_token' => 'user'
+      }
+    end
+    let(:user1) { FiChat::Member::User.new(user_params1) }
+    let(:admin_params1) do
+      {
+        'id' => 1,
+        'last_name' => 'Carlton',
+        'first_name' => 'Larry',
+        'access_token' => 'admin'
+      }
+    end
+    let(:admin1) { FiChat::Member::Admin.new(admin_params1) }
+    let(:subject_method){ ChatDirectWithAdminRoom.find_or_create_by(user1, admin1) }
+    it { expect(subject_method).to be_kind_of(ChatDirectWithAdminRoom) }
+    context 'Room does not exist' do
+      it 'stores a new room' do
+        expect { subject_method }.to change(ChatDirectWithAdminRoom, :count).by(1)
+      end
+    end
+
+    context 'Room already exists' do
+      let!(:chat_room) { ChatDirectWithAdminRoom.create(user_id: user1.id, admin_id: admin1.id) }
+      it 'does not store a new room' do
+        expect {  subject_method }.to change(ChatDirectWithAdminRoom, :count).by(0)
+      end
+    end
+  end
 end
