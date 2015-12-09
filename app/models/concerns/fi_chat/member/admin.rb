325cbedeb98bf09803282340047a30863589138d
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-09 10:12:36 +0900

    Implement method to instantiate admin

--- app/models/concerns/fi_chat/member/admin.rb
+++ app/models/concerns/fi_chat/member/admin.rb
@@ -1,11 +1,21 @@
 module FiChat
   class Member
     class Admin < FiChat::Member
-
       def type
         'Admin'
       end
 
+      class << self
+        def find(id, access_token)
+          admin = RequlMobileAdminApi.new(access_token).admin_info(id)
+          ::FiChat::Member::Admin.new(admin)
+        end
+
+        def find_list(ids, access_token)
+          admins = RequlMobileAdminApi.new(access_token).admins(ids)
+          Array(admins['admins']).map { |a| ::FiChat::Member::Admin.new(a) }
+        end
+      end
     end
   end
 end
