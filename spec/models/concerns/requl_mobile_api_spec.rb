f62a4ce33f32348578a00befbf7e8424e10218bb
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-11 17:42:45 +0900

    Add access_token to spec

--- spec/models/concerns/requl_mobile_api_spec.rb
+++ spec/models/concerns/requl_mobile_api_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 describe RequlMobileAdminApi do
+  let(:access_token) { 'accesstoken' }
   let(:admin) { { 'admin' => { 'id' => 1, 'first_name' => 'first_name', 'last_name' => 'last_name' } } }
+  let(:admins) do
+    { 'admins' =>
+      [
+        { 'id' => 1, 'first_name' => 'first_name1', 'last_name' => 'last_name1' },
+        { 'id' => 2, 'first_name' => 'first_name2', 'last_name' => 'last_name2' }
+      ]
+    }
+  end
 
   describe '.admin_info' do
     context 'request admin_id is exist' do
@@ -9,9 +18,9 @@ describe RequlMobileAdminApi do
         allow_any_instance_of(RequlMobileAdminApi).to receive(:request)
           .with(:get,
                 "#{RequlMobileAdminApi::INTERNAL_BASE_URL}/v1/admins/#{admin['admin']['id']}",
-                { application_token: RequlMobileAdminApi::APPLICATION_TOKEN })
+                { access_token: access_token, application_token: RequlMobileAdminApi::APPLICATION_TOKEN })
           .and_return(admin)
-        ret = RequlMobileAdminApi.new.admin_info(admin['admin']['id'])
+        ret = RequlMobileAdminApi.new(access_token).admin_info(admin['admin']['id'])
         expect(ret).to eq admin['admin']
       end
     end
@@ -20,11 +29,25 @@ describe RequlMobileAdminApi do
         allow_any_instance_of(RequlMobileAdminApi).to receive(:request)
           .with(:get,
                 "#{RequlMobileAdminApi::INTERNAL_BASE_URL}/v1/admins/#{admin['admin']['id']}",
-                { application_token: RequlMobileAdminApi::APPLICATION_TOKEN })
+                { access_token: access_token, application_token: RequlMobileAdminApi::APPLICATION_TOKEN })
           .and_return({})
         expect {
-          RequlMobileAdminApi.new.admin_info(admin['admin']['id'])
+          RequlMobileAdminApi.new(access_token).admin_info(admin['admin']['id'])
         }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe '.admins' do
+    context 'request admin_ids is exist' do
+      it 'response has admin' do
+        allow_any_instance_of(RequlMobileAdminApi).to receive(:request)
+          .with(:get,
+                "#{RequlMobileAdminApi::INTERNAL_BASE_URL}/v1/admins/list",
+                { admin_ids: [1, 2], access_token: access_token, application_token: RequlMobileAdminApi::APPLICATION_TOKEN })
+          .and_return(admins)
+        ret = RequlMobileAdminApi.new(access_token).admins([admins['admins'][0]['id'], admins['admins'][1]['id']])
+        expect(ret).to eq admins
       end
     end
   end
