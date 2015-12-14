b6c3a32595b6beff7c57bbaaaddd9093125c9d31
yoshihiro asada <yoshihiro0921@gmail.com>
2015-12-14 16:49:32 +0900

    Implement POST /v1/admins/rooms API to create chat_direct_with_admin_room

--- app/apis/public/v1/admins/rooms.rb
+++ app/apis/public/v1/admins/rooms.rb
@@ -1,14 +1,30 @@
 module Public
   module V1
-    module Chats
-      module Admins
-        class Rooms < Grape::API
+    class Admins::Rooms < Grape::API
+      params do
+        requires :access_token, type: String
+      end
+      namespace :admins do
+        resources :rooms do
           get '' do
             case
             when "Admin"
               # @未実装 => adminの情報をgetするinternal apiの実装とapi clientの実装の必要
               @chat_room = ChatDirectWithAdminRoom.find_or_create_by(user_id: me.id, admin_id: 1).chat_room_index_cache
             end
+          end
+
+          desc 'Start direct chat'
+          params do
+            requires :user_id, type: Integer
+            requires :admin_id, type: Integer
+          end
+          post '/', rabl: 'public/v1/admins/rooms/create' do
+            user = FiChat::Member::User.find_list([params[:user_id]]).first
+            admin = FiChat::Member::Admin.find(params[:admin_id], params[:access_token])
+            fail ActionController::BadRequest if user.blank? || admin.blank?
+
+            @chat_room = ChatDirectWithAdminRoom.find_or_create_by(user, admin)
           end
         end
       end
---
+++ app/views/apis/public/v1/admins/rooms/create.rabl
@@ -0,0 +1,5 @@
+object false
+
+child(@chat_room, root: :chat_room, object_root: false) do
+  attributes :user_id, :admin_id
+end
